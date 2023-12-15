19b6589b7f1a0f80cf3c7f10f8e561e2ab8ef012
TC <nothing@nothing.com>
2023-12-16 06:10:39 +1300

    Added game loop. Added score logic.

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
 const choices = ['paper','scissors','rock'];
+const whoBeats = {
+  'paper': 'rock',
+  'rock': 'scissors',
+  'scissors': 'paper'
+}
+let score = {
+  wins: 0,
+  losses: 0,
+  draws: 0
+}
 
 function getComputerChoice(){
   return choices[Math.floor(Math.random()*3)];
@@ -7,39 +17,45 @@ function getComputerChoice(){
 function getPlayerChoice(){
   let playerChoice;
   while(true){
-    playerChoice = prompt("Enter 'paper', 'scissors' or 'rock'");
-    if (playerChoice === 'q') break;
-    if (choices.includes(playerChoice)) break;
+    playerChoice = prompt("Enter:\n'paper', 'scissors' or 'rock'\nor press 'q' to quit.")
+    if (choices.includes(playerChoice) || playerChoice === 'q' || playerChoice === null) break;
   }
   return playerChoice;
 }
 
 function playRound(playerChoice, computerChoice){
-  return;
+  if ( whoBeats[playerChoice] === computerChoice) {
+    console.log(`You Win! ${playerChoice} beats ${computerChoice}.`)
+    score.wins += 1;
+  } else if ( whoBeats[computerChoice] === playerChoice) {
+    console.log(`You lose! ${computerChoice} beats ${playerChoice}.`)
+    score.losses += 1;
+  } else {
+    console.log(`Draw! Both picked ${playerChoice}`);
+    score.draws += 1;
+  }
+  score.total += 1;
 }
 
+function displayScore(prefix = "Score"){
+  console.log(
+    `${prefix}:
+     ${score.wins} wins,
+     ${score.draws} ties,
+     ${score.losses} losses`)
+}
 
 
 // ----- Code Execution starts here -----
-function main(){
-  let playingGame = true;
-  while (playingGame) {
+function game(){
+  while (true) {
     let computerChoice = getComputerChoice();
     let playerChoice = getPlayerChoice();
-    break;
+    if (playerChoice === null || playerChoice === 'q') break;
+    playRound(playerChoice,computerChoice);
+    displayScore("Score");
   }
+  displayScore("Final Score")
 }
 
-main();
-
-
-// let obj = {
-//     'paper': 0,
-//     'scissors': 0,
-//     'rock': 0}
-
-// for(let i = 0; i < 10000; i++) {
-//     obj[getComputerChoice()] += 1;
-// }
-
-// console.log(obj);
+game();

faad5e749597c04ba1b83bb3acf3726bfdeb278d
TC <nothing@nothing.com>
2023-12-23 09:01:32 +1300

    Round of 5 rules added and play again button.

--- code.js
+++ code.js
@@ -4,7 +4,9 @@ const whoBeats = {
   'rock': 'scissors',
   'scissors': 'paper'
 }
+
 let score = {
+  round: 0,
   wins: 0,
   losses: 0,
   draws: 0
@@ -27,14 +29,15 @@ function playRound(playerChoice, computerChoice){
   if ( whoBeats[playerChoice] === computerChoice) {
     console.log(`You Win! ${playerChoice} beats ${computerChoice}.`)
     score.wins += 1;
+    score.round += 1;
   } else if ( whoBeats[computerChoice] === playerChoice) {
     console.log(`You lose! ${computerChoice} beats ${playerChoice}.`)
     score.losses += 1;
+    score.round += 1;
   } else {
     console.log(`Draw! Both picked ${playerChoice}`);
     score.draws += 1;
   }
-  score.total += 1;
 }
 
 
@@ -49,10 +52,39 @@ function playGame(playerChoice){
   let computerChoice = getComputerChoice();
   playRound(playerChoice,computerChoice);
   displayScore();
+  if (score.round == 5) playAgain();
 }
 
 function displayScore(){
   const results = document.querySelector('.results');
-  results.innerText = `Score: ${score.wins} wins, ${score.draws} ties, ${score.losses} losses`;
-  
+  if (score.round === 5) {
+    const winner = determineWinner();
+    results.innerText = `${winner}\nFinal Score: ${score.wins} wins, ${score.draws} ties, ${score.losses} losses`;
+  } else {
+    results.innerText = `Score: ${score.wins} wins, ${score.draws} ties, ${score.losses} losses`;
+  }
+}
+
+function determineWinner(){
+  if (score.wins > score.losses) {
+    return 'You Win!'
+  } else {
+    return 'AI Wins...'
+  }
+}
+
+function playAgain(){
+  btnContainer.childNodes.forEach(node => node.disabled = true)
+
+  const playAgainBtn = document.createElement('button');
+  playAgainBtn.innerText = "Play Again?";
+  document.querySelector('.play-again').appendChild(playAgainBtn);
+
+  playAgainBtn.addEventListener('click', () => {
+    btnContainer.childNodes.forEach(node => node.disabled = false);
+    [score.draws, score.losses, score.round, score.wins] = [0,0,0,0];
+    document.querySelector('.results').innerText = "";
+    playAgainBtn.remove();
+  }, {once: true})
+
 }
